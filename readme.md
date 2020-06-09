0ed6220af8cd0458df2bfa409281b34677c796d3
Andrew Lisowski <lisowski54@gmail.com>
2020-06-09 13:10:28 -0700

    speed up + add docs

--- readme.md
+++ readme.md
@@ -16,13 +16,29 @@ yarn add -D react-docgen-typescript-plugin
 ## Usage
 
 ```ts
-const ReactDocgenTypescriptPlugin = require('react-docgen-typescript-plugin');
+const ReactDocgenTypescriptPlugin = require("react-docgen-typescript-plugin");
 
 module.exports = {
-  plugins: [new ReactDocgenTypescriptPlugin()],
+  plugins: [
+    new ReactDocgenTypescriptPlugin(),
+    // or with options
+    new ReactDocgenTypescriptPlugin({ tsconfigPath: "./tsconfig.json" }),
+  ],
 };
 ```
 
+### Options
+
+This plugins support all parser options from [react-docgen-typescript](https://github.com/styleguidist/react-docgen-typescript#parseroptions) and all of the following options
+
+| Option               | Type           | Description                                                                                                                                         | Default                   |
+| -------------------- | -------------- | --------------------------------------------------------------------------------------------------------------------------------------------------- | ------------------------- |
+| tsconfigPath         | string         | Specify the location of the `tsconfig.json` to use.                                                                                                 | `null`                    |
+| docgenCollectionName | string or null | Specify the docgen collection name to use. All docgen information will be collected into this global object. Set to `null` to disable.              | `STORYBOOK_REACT_CLASSES` |
+| setDisplayName       | boolean        | Set the components' display name. If you want to set display names yourself or are using another plugin to do this, you should disable this option. | `true`                    |
+| typePropName         | string         | Specify the name of the property for docgen info prop type.                                                                                         | `type`                    |
+
 ## Prior Art
 
-- [sn-client](https://github.com/SenseNet/sn-client/pull/113/files) - Inspired by this custom webpack plugin
+- [sn-client](https://github.com/SenseNet/sn-client/) - Inspired by this custom webpack plugin
+- [react-docgen-typescript-loader](https://github.com/strothj/react-docgen-typescript-loader/) - Webpack loader to generate docgen information from Typescript React components.
--- src/index.ts
+++ src/index.ts
@@ -2,84 +2,62 @@ import path from "path";
 import * as webpack from "webpack";
 import ts from "typescript";
 import docGen from "react-docgen-typescript";
-import { StaticPropFilter } from "react-docgen-typescript/lib/parser";
 import generateDocgenCodeBlock from "react-docgen-typescript-loader/dist/generateDocgenCodeBlock.js";
 
-export type PluginOptions = docGen.ParserOptions &
-  StaticPropFilter & {
-    /**
-     * Specify the location of the tsconfig.json to use. Can not be used with
-     * compilerOptions.
-     **/
-    tsconfigPath?: string;
+interface LoaderOptions {
+  /**
+   * Specify the location of the tsconfig.json to use. Can not be used with
+   * compilerOptions.
+   **/
+  tsconfigPath?: string;
 
-    /**
-     * Specify the docgen collection name to use. All docgen information will
-     * be collected into this global object. Set to null to disable.
-     *
-     * @default STORYBOOK_REACT_CLASSES
-     * @see https://github.com/gongreg/react-storybook-addon-docgen
-     **/
-    docgenCollectionName?: string | null;
+  /**
+   * Specify the docgen collection name to use. All docgen information will
+   * be collected into this global object. Set to null to disable.
+   *
+   * @default STORYBOOK_REACT_CLASSES
+   * @see https://github.com/gongreg/react-storybook-addon-docgen
+   **/
+  docgenCollectionName?: string | null;
 
-    /**
-     * Automatically set the component's display name. If you want to set display
-     * names yourself or are using another plugin to do this, you should disable
-     * this option.
-     *
-     * ```
-     * class MyComponent extends React.Component {
-     * ...
-     * }
-     *
-     * MyComponent.displayName = "MyComponent";
-     * ```
-     *
-     * @default true
-     */
-    setDisplayName?: boolean;
+  /**
+   * Automatically set the component's display name. If you want to set display
+   * names yourself or are using another plugin to do this, you should disable
+   * this option.
+   *
+   * ```
+   * class MyComponent extends React.Component {
+   * ...
+   * }
+   *
+   * MyComponent.displayName = "MyComponent";
+   * ```
+   *
+   * @default true
+   */
+  setDisplayName?: boolean;
 
-    /**
-     * Specifiy the name of the property for docgen info prop type.
-     *
-     * @default "type"
-     */
-    typePropName?: string;
-  };
+  /**
+   * Specify the name of the property for docgen info prop type.
+   *
+   * @default "type"
+   */
+  typePropName?: string;
+}
+
+export type PluginOptions = docGen.ParserOptions & LoaderOptions;
 
 function processModule(
-  {
-    tsconfigPath,
-    propFilter,
-    componentNameResolver,
-    shouldExtractLiteralValuesFromEnum,
-    shouldRemoveUndefinedFromOptional,
-    savePropValueAsString,
-    skipPropsWithName,
-    skipPropsWithoutDoc,
-    ...loaderOptions
-  }: PluginOptions,
+  parser: docGen.FileParser,
   module: any,
-  tsProgram: ts.Program
+  tsProgram: ts.Program,
+  loaderOptions: PluginOptions
 ) {
   if (!module) {
     return;
   }
 
-  const docgenOptions = {
-    propFilter,
-    componentNameResolver,
-    shouldExtractLiteralValuesFromEnum,
-    shouldRemoveUndefinedFromOptional,
-    savePropValueAsString,
-    skipPropsWithName,
-    skipPropsWithoutDoc,
-  };
-
-  const docgenParser = tsconfigPath
-    ? docGen.withCustomConfig(tsconfigPath, docgenOptions)
-    : docGen.withDefaultConfig(docgenOptions);
-  const componentDocs = docgenParser.parseWithProgramProvider(
+  const componentDocs = parser.parseWithProgramProvider(
     module.userRequest,
     () => tsProgram
   );
@@ -104,6 +82,7 @@ function processModule(
 }
 
 export default class DocgenPlugin {
+  private name = "React Docgen Typescript Plugin";
   private options: PluginOptions;
 
   constructor(options: PluginOptions) {
@@ -112,9 +91,25 @@ export default class DocgenPlugin {
 
   apply(compiler: webpack.Compiler) {
     const pathRegex = RegExp(`\\${path.sep}src.+\.tsx`);
+    const {
+      tsconfigPath,
+      propFilter,
+      componentNameResolver,
+      shouldExtractLiteralValuesFromEnum,
+      shouldRemoveUndefinedFromOptional,
+      savePropValueAsString,
+      ...loaderOptions
+    } = this.options;
+    const docgenOptions = {
+      propFilter,
+      componentNameResolver,
+      shouldExtractLiteralValuesFromEnum,
+      shouldRemoveUndefinedFromOptional,
+      savePropValueAsString,
+    };
 
-    compiler.hooks.compilation.tap("DocgenPlugin", (compilation) => {
-      compilation.hooks.seal.tap("DocgenPlugin", () => {
+    compiler.hooks.compilation.tap(this.name, (compilation) => {
+      compilation.hooks.seal.tap(this.name, () => {
         const modulesToProcess: any[] = [];
 
         compilation.modules.forEach((module) => {
@@ -136,9 +131,12 @@ export default class DocgenPlugin {
             target: ts.ScriptTarget.Latest,
           }
         );
+        const parser = tsconfigPath
+          ? docGen.withCustomConfig(tsconfigPath, docgenOptions)
+          : docGen.withDefaultConfig({});
 
         modulesToProcess.forEach((m) =>
-          processModule(this.options, m, tsProgram)
+          processModule(parser, m, tsProgram, loaderOptions)
         );
       });
     });
