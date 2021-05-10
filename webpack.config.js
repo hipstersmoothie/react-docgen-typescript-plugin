48ff075309bf636411003482cb96d7f9bf9ded73
Juho Vepsäläinen <bebraw@gmail.com>
2021-05-10 13:13:29 +0300

    chore: Use Column example

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const { ReactDocgenTypeScriptPlugin } = require("./");
 
 module.exports = {
   mode: "development",
-  entry: { main: "./src/__tests__/__fixtures__/Simple.tsx" },
+  entry: { main: "./src/__tests__/__fixtures__/Column.tsx" },
   output: {
     path: path.join(__dirname, "./example-dist"),
   },
