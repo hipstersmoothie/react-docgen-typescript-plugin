a3965f6ad353d1beb6266c7336be2f0b1af01468
Juho Vepsäläinen <bebraw@gmail.com>
2021-05-11 14:03:06 +0300

    fix: Use NullDependency

--- src/dependency.ts
+++ src/dependency.ts
@@ -5,7 +5,7 @@ import * as docGen from "react-docgen-typescript";
 
 // eslint-disable-next-line
 // @ts-ignore: What's the right way to refer to this one?
-import ModuleDependency from "webpack/lib/dependencies/ModuleDependency.js";
+import Module from "webpack/lib/Module.js";
 
 // eslint-disable-next-line
 // @ts-ignore: What's the right way to refer to this one?
@@ -13,25 +13,13 @@ import makeSerializable from "webpack/lib/util/makeSerializable.js";
 
 // eslint-disable-next-line
 // @ts-ignore TODO: Figure out where to find a typed version
-import Dependency from "webpack/lib/Dependency.js";
+import NullDependency from "webpack/lib/dependencies/NullDependency.js";
 
 import { generateDocgenCodeBlock } from "./generateDocgenCodeBlock";
 import { LoaderOptions } from "./types";
 
-class DocGenDependency extends ModuleDependency {
-  public static Template: ModuleDependency.Template;
-
-  getReferencedExports(): [] {
-    return Dependency.NO_EXPORTS_REFERENCED;
-  }
-
-  get type(): string {
-    return "docgen";
-  }
-
-  get category(): string {
-    return "docs";
-  }
+class DocGenDependency extends NullDependency {
+  public static Template: NullDependency.Template;
 
   // TODO: Note if you want that modules correctly invalidate and cache you need to add updateHash to your Dependency and hash the type info (because that might change depending on outside factors (other modules)
 }
@@ -43,7 +31,7 @@ type Options = {
   docgenOptions: LoaderOptions;
 };
 
-class DocGenTemplate extends ModuleDependency.Template {
+class DocGenTemplate extends NullDependency.Template {
   private options: Options;
 
   constructor(options: Options) {
@@ -52,8 +40,12 @@ class DocGenTemplate extends ModuleDependency.Template {
     this.options = options;
   }
 
-  apply(dependency: Dependency, source: ReplaceSource): void {
-    const { userRequest } = dependency;
+  apply(
+    dependency: NullDependency,
+    source: ReplaceSource,
+    { module }: { module: Module }
+  ): void {
+    const { userRequest } = module;
 
     // Since parsing fails with a path including !, remove it.
     // The problem is that webpack injects that and there doesn't
